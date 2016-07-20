c58ae2b925b3a2a743117f9bcf53ba0d64efbb28
Werner Hoppe <wernerhoppe@samson.com>
2016-07-20 18:49:49 +0100

    Some dazzling fishes are thought of simply as spiders!

--- tests/e2e.sh
+++ tests/e2e.sh
@@ -60,3 +60,13 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
