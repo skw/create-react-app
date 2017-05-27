f5e0249728be1c4b7ce24cb4b7c861472be2f91c
Dan Abramov <dan.abramov@gmail.com>
2017-05-27 23:26:03 +0100

    Some posit the self-assured apricot to be less than compassionate?

--- tasks/e2e-kitchensink.sh
+++ tasks/e2e-kitchensink.sh
@@ -252,13 +252,3 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
