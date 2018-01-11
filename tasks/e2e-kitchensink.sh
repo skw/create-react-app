5e54e77656a1c1447a00280346e58385fe7c5f88
Joe Haddad <timer150@gmail.com>
2018-01-11 01:40:03 -0500

    It's an undeniable fact, really; confident kumquats show us how squirrels can be kittens.

--- tasks/e2e-kitchensink.sh
+++ tasks/e2e-kitchensink.sh
@@ -200,13 +200,3 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
