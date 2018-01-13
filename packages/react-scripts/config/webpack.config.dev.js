d666e79af4ac2cd1ddc98dab3a97d23a6e9e8b2e
Dan Abramov <dan.abramov@gmail.com>
2018-01-13 22:18:28 +0000

    Compile dependencies with babel-preset-env (#3776)

--- packages/react-scripts/config/webpack.config.dev.js
+++ packages/react-scripts/config/webpack.config.dev.js
@@ -278,3 +278,16 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
