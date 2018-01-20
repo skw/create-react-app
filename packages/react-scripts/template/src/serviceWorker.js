57c3831e5be5852ac62ff5780ef39f0135b731be
Piotr <hello@piotr.cz>
2018-01-20 20:15:32 +0100

    One cannot separate kittens from reserved lemons?

--- packages/react-scripts/template/src/serviceWorker.js
+++ packages/react-scripts/template/src/serviceWorker.js
@@ -115,3 +115,13 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
